500fe9fcbeb73e1ee99ba73b77cc7dc2520b771b
Patrik Arell <patrikarell@gmail.com>
2024-02-08 14:45:25 +0100

    connection to player mySql

--- main.js
+++ main.js
@@ -2,28 +2,35 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = 3000;
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
 
-var bodyParser = require('body-parser')
+const { sequelize, Player } = require('./models')
+const migrationhelper = require('./migrationhelper')
+
+var bodyParser = require('body-parser');
+const player = require('./models/player');
 
 app.use(bodyParser.json())
 app.use(cors())
 
-const players = [{
-    name: "Patrik",
-    jersey: 29,
-    age:35,
-    id:1
-},{
-name: "Foppa",
-jersey: 13,
-age: 52,
-id:2
-},{
-    name:"Sudden",
-    jersey: 13,
-    age:52,
-    id:3
-}]
+// const players = [{
+//     name: "Patrik",
+//     jersey: 29,
+//     age:35,
+//     id:1
+// },{
+// name: "Foppa",
+// jersey: 13,
+// age: 52,
+// id:2
+// },{
+//     name:"Sudden",
+//     jersey: 13,
+//     age:52,
+//     id:3
+// }]
 
 app.get('/api/players/:anvId', (req, res) => {
     console.log(req.params.anvId)
@@ -36,6 +43,7 @@ if(p == undefined){
 }
 res.json(p)
 })
+
 
 function getNextId(){
     let m = Math.max(...players.map(player => player.id))
@@ -54,8 +62,8 @@ console.log(req.body)
 res.status(201).send('Created')
 });
 
-app.put('/api/players',(req,res)=>{
- let p = employees.find(employee=>employee.id == req.params.anvId)
+app.put('/api/players/:anvId',(req,res)=>{
+ let p = players.find(player=>player.id == req.params.anvId)
  if(p == undefined){
      res.status(404).send('Finns inte')
  }
@@ -65,15 +73,15 @@ app.put('/api/players',(req,res)=>{
  res.status(204).send('Updated')
 });
 
-app.delete('/api/players',(req,res)=>{
+app.delete('/api/players/:anvId',(req,res)=>{
     console.log(req.params.anvId)
-    let p = employees.find(employee=>employee.id == req.params.anvId)
+    let p = players.find(player=>player.id == req.params.anvId)
     // 404???
     if(p == undefined){
         res.status(404).send('Finns inte')
     }
-    employees.splice(employees.indexOf(p),1)
-    res.status(204).send('')   
+    players.splice(players.indexOf(p),1)
+    res.status(204).send('deleted')   
 });
 
 
@@ -95,3 +103,65 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
   
+
+async function listAll() {
+    const players = await Player.findAll();
+    for (const player of players) {
+      console.log('************************');
+      console.log('ID:', player.id);
+      console.log('Name:', player.name);
+    }
+  }
+  async function createNew() {
+    console.log('** NEW ** ');
+    const name = await rl.question('Name:');
+    const jersey = await rl.question('Jersey:');
+    Player.create({
+      name: name,
+      jersey:jersey,
+    
+    });
+  }
+  async function main() {
+    await migrationhelper.migrate()
+    
+    const thePlayer = await Player.findOne({
+        where: { id:2 }
+    })
+    thePlayer.name = 'Sudden'
+    thePlayer.jersey = '13'
+
+    await thePlayer.save()
+
+    while (true) {
+        await migrationhelper.migrate();
+        console.log('1. Lista alla players');
+        console.log('2. Skapa player');
+        console.log('3. Uppdatera player');
+        console.log('4. Ta bort player');
+        console.log('9. Avsluta');
+    
+        const sel = await rl.question('Val:');
+        if (sel == '1') {
+          await listAll();
+        }
+        if (sel == '2') {
+          await createNew();
+        }
+        if (sel == '3') {
+          updateOne();
+        }
+        if (sel == '4') {
+          deleteOne();
+        }
+        if (sel == '9') {
+          break;
+        }
+      }
+    }
+    
+    (async () => {
+      main();
+    })();
+   
+    // ---- //
